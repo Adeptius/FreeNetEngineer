6f0d63b53de1c54a8035bffa08f507bcf1febfa8
Vladimir <adeptius@gmail.com>
2016-09-13 18:04:44 +0300

    prepare to refactoring database

--- app/src/test/java/ua/adeptius/myapplications/LoginTest.java
+++ app/src/test/java/ua/adeptius/myapplications/LoginTest.java
@@ -1,22 +1,25 @@
 package ua.adeptius.myapplications;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import ua.adeptius.myapplications.connection.Network;
-import ua.adeptius.myapplications.util.Utilites;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class LoginTest {
 
     @Test
-    public void loginRightTest(){
+    public void loginFalseTest(){
         assertFalse(Network.isAuthorizationOk("FalseLogin", "FalsePassword"));
     }
+
+    @Test
+    public void loginOkTest(){
+        if (!Network.isAuthorizationOk("xactive", "36e1a5072c78359066ed7715f5ff3da85b4eadaed0662599d2f1cae336757aa0")){
+            assertTrue(Network.isAuthorizationOk("xactive", "780e26bf8d6b06e923ccdac8b2d2f5295b4eadaed0662599d2f1cae336757aa0"));
+        }
+    }
+
 
 
 }

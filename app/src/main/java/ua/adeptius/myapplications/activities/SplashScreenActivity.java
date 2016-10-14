ee4a5f85c09eb75fc05820e623bcf5f5f95adefe
Vladimir <adeptius@gmail.com>
2016-10-14 09:44:49 +0300

    Splash screen impruved

--- app/src/main/java/ua/adeptius/myapplications/activities/SplashScreenActivity.java
+++ app/src/main/java/ua/adeptius/myapplications/activities/SplashScreenActivity.java
@@ -9,7 +9,9 @@ import android.widget.ImageView;
 
 
 import ua.adeptius.myapplications.R;
-import ua.adeptius.myapplications.util.Utilites;
+
+import static ua.adeptius.myapplications.util.Utilites.EXECUTOR;
+import static ua.adeptius.myapplications.util.Utilites.HANDLER;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -18,16 +20,30 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splash_screen_animation);
-        ImageView imageView = (ImageView) findViewById(R.id.runnerView);
-        imageView.startAnimation(anim);
+        EXECUTOR.submit(new Runnable() {
+            @Override
+            public void run() {
+                Animation anim = AnimationUtils.loadAnimation(SplashScreenActivity.this,
+                        R.anim.splash_screen_animation);
+                final ImageView imageView = (ImageView) findViewById(R.id.runnerView);
+                imageView.startAnimation(anim);
+                try {
+                    Thread.sleep(1300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Animation animGone = AnimationUtils.loadAnimation(SplashScreenActivity.this,
+                        R.anim.splash_screen_animation_gone);
+                imageView.startAnimation(animGone);
+            }
+        });
 
-        Utilites.HANDLER.postDelayed(new Runnable() {
+        HANDLER.postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
                 finish();
             }
-        }, 1500);
+        }, 1700);
     }
 }

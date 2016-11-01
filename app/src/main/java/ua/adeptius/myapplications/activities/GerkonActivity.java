873da28d774ae3a602a43b19b00d5dd30edf3bf4
Vladimir <adeptius@gmail.com>
2016-11-01 12:05:38 +0300

    gerkon activity added

---
+++ app/src/main/java/ua/adeptius/myapplications/activities/GerkonActivity.java
@@ -0,0 +1,15 @@
+package ua.adeptius.myapplications.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ua.adeptius.myapplications.R;
+
+public class GerkonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerkon);
+    }
+}
